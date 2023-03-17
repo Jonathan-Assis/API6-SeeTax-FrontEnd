ea6a3260612b6a2c8100446f79055e4f1c043faa
Sarah781 <sarah7bvs@gmail.com>
2023-03-17 17:03:03 -0300

    Adicionando barra de pesquisa

---
+++ src/components/BarraDePesquisa.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Barra de Pesquisa">
+                <input type="text" class="input" placeholder="Pesquise" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.box {
+    background: #136d2a;
+}
+</style>
