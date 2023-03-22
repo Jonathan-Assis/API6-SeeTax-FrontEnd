ac857615f12fc817b1fe18d0cca6564a4e7b4be2
Raul Santos Iglesias <51235779+RaulIglesias@users.noreply.github.com>
2023-03-22 02:09:03 -0300

    Sidebar Structure - Initial Commit
    Estruturando Sidebar, por enquanto sem links.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 
 <template>  
-    <TableStructure/>
+
+    <Sidebar/>
+      <!--Main Navigation-->
+  
+  <!--Main layout-->
+  <main style="margin-top: 5px;">
+    <div class="container-fluid"></div>
+
+     <TableStructure />
+
+  </main>
+  <!--Main layout-->
   
 </template>
 
 
 <script> 
   import TableStructure from  './components/TableStructure.vue'
+  import Sidebar from './components/Sidebar.vue'
  
 
   export default {
     name: 'app',
     components: {
-      TableStructure
+      TableStructure,
+      Sidebar
     }
   }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -7,7 +17,7 @@ import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 
 import "bootstrap/dist/js/bootstrap.js"
 
