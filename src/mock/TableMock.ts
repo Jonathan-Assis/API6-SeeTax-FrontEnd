dd364c50a185abdca46edd8a87c054854e2cee8e
Rita2022 <rita.ferreira2529@gmail.com>
2023-03-26 11:36:56 -0300

    Alterando dados

---
+++ src/mock/TableMock.ts
@@ -0,0 +1,41 @@
+export const tabledata = [
+    {
+        banco: 'Itaú Unibanco S.A.',
+        tipo: 'Conta Poupança',
+        dataAtualizada:'20/03/2023 12:00',
+        media:'3,00',
+        quantidadeServico:57,
+    },
+    {
+        banco: 'Banco Itaúcard S.A',
+        tipo: 'Conta Pagamento Pré Pago',
+        dataAtualizada:'20/03/2023 12:00',
+        media:'3,10',
+        quantidadeServico:30
+        ,
+    },
+    {
+        banco: 'Lorem Ipsum',
+        tipo: 'Conta Corrente',
+        dataAtualizada:'20/03/2023 12:00',
+        media:'5,50',
+        quantidadeServico:25
+        ,
+    },
+    {
+        banco: 'Lorem Ipsum',
+        tipo: 'Conta Corrente',
+        dataAtualizada:'20/03/2023 12:00',
+        media:'5,80',
+        quantidadeServico:22
+        ,
+    },
+    {
+        banco: 'Lorem Ipsum',
+        tipo: 'Conta Corrente',
+        dataAtualizada:'20/03/2023 12:00',
+        media:'6,00',
+        quantidadeServico:20
+        ,
+    },
+]
