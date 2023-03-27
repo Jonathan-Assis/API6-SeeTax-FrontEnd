e93c06f9200562dc7b62cfba4264687152f429a0
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-27 01:45:53 -0300

    Criação do component de title outside :jigsaw:

---
+++ src/components/title/TitleOutsideComponent.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name:'TitleOutsideComponent',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        icon: {
+            type: String,
+        }
+    },
+})
+</script>
+
+<template>
+    <header>
+        <slot name='icon'></slot>
+        <h5>{{ title }}</h5>
+    </header>
+</template>
+
+<style scoped>
+header {
+    display: flex;
+    margin: 10px 0;
+    gap: 12px;
+}
+
+h5 {
+    color: var(--st-text-primary-black-color);
+}
+</style>
