959ad51e5f2f7a5aae72a41040d4312b01535b9b
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-27 01:46:58 -0300

    Ajustes na responsividade da router view
    Com base no cálculo da sidebar é feito o reajuste do router view

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,36 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { PhHeart } from '@phosphor-icons/vue'
+
+import SideBarComponent from './components/sidebar/SideBarComponent.vue';
+import { sidebarWidth } from './components/sidebar/SideBarState';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    SideBarComponent,
+    PhHeart
+  },
+  setup () {
+
+    return {
+      sidebarWidth
+    }
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <main class="container-main">
+    <SideBarComponent />
+    <div class="container-router-view" :style="{ 'margin-left': sidebarWidth }">
+      <router-view></router-view>
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container-view {
+  padding: 0 30px 0 70px
 }
 </style>
