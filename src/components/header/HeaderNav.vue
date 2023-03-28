5f1d9bdaea1da55f7f9a82a167c27dfd8e3922f8
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-28 01:04:11 -0300

    Criação da navegação em header via breadcrumb

---
+++ src/components/header/HeaderNav.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { MDBBreadcrumbItem } from 'mdb-vue-ui-kit';
+
+export default defineComponent({
+    name:'HeaderNav',
+    components: {
+        MDBBreadcrumbItem
+    },
+    props: {
+        to: {
+            type: String,
+            required: true
+        },
+        routerName: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
+
+<template>
+    <MDBBreadcrumbItem class="breadcrumb-item">
+        <router-link :to="to">
+            {{ routerName }}
+        </router-link>
+    </MDBBreadcrumbItem>
+</template>
