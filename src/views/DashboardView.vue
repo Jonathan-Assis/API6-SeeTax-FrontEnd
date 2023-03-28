0dc427dcfd24369bc3af7e1ce8e7d1f6ae7d0d03
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-28 01:07:20 -0300

    Adição do component header

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,22 +1,31 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import HeaderComponent from '../components/header/headerComponent.vue';
 import TableComponent from  '../components/table/TableComponent.vue'
 import TitleOutsideComponent from '../components/title/TitleOutsideComponent.vue';
 import { PhPackage, PhTicket, PhTrophy } from '@phosphor-icons/vue'
+import { MDBBreadcrumbItem } from 'mdb-vue-ui-kit';
+import HeaderNav from '../components/header/HeaderNav.vue';
 
 
 export default defineComponent({
     name: 'DashboardView',
     components: {
+    HeaderComponent,
+    HeaderNav,
     TitleOutsideComponent,
     TableComponent,
     PhTrophy,
-}
+    MDBBreadcrumbItem
+    },
+    setup() {
+    }
 })
 
 </script>
 
 <template>
+    <HeaderComponent :isDashboard="true" />
     <TitleOutsideComponent title="Ranking">
         <template v-slot:icon>
             <PhTrophy :size="23" class="st-icon-gray" weight="duotone" />
@@ -24,3 +33,6 @@ export default defineComponent({
     </TitleOutsideComponent>
     <TableComponent />
 </template>
+
+<style scoped>
+</style>
