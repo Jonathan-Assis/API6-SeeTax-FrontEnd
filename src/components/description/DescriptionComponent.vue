762cd4585fc96dadca7a287abdbe67ac79e4516c
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-28 01:03:25 -0300

    Criação do component de descrição :jigsaw:

---
+++ src/components/description/DescriptionComponent.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'DescriptionComponent',
+    props:{
+        title: {
+            type: String,
+            required: true,
+        }
+    },
+    setup() {
+        
+    },
+})
+</script>
+
+<template>
+    <div class="description">
+      {{ title }}
+    </div>
+</template>
+
+<style scoped>
+
+.description {
+  height: 4rem;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  border-top-left-radius: 7px;
+  border-top-right-radius: 7px;
+}
+</style>
