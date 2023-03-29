3da24663320f1c8b28f9c4134715c6f34ebdb0d9
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-28 23:24:41 -0300

    Criação de interface de tabelas

--- src/interfaces/components/ITable.ts
+++ src/interfaces/components/ITable.ts
@@ -1,6 +1,13 @@
-export interface ITableRanking {
+export interface ITable {
+    data: [];
+}
+
+export interface ITableRankingBody {
     id: number;
     name: string;
     type: string;
     average: number;
+}
+export interface ITableRankingHeader {
+    header: ['Posição', 'Tipo', 'Data de Atualização', 'Média de Tarifas']
 }
