6d75dc78a9dabb3e0c7d843f3c46fab48a407e8a
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-28 23:22:45 -0300

    Alteração na props e adição de classe de estilo global

--- src/components/description/DescriptionComponent.vue
+++ src/components/description/DescriptionComponent.vue
@@ -2,33 +2,29 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'DescriptionComponent',
-    props:{
-        title: {
-            type: String,
-            required: true,
-        }
-    },
-    setup() {
-        
-    },
+  name: 'DescriptionComponent',
+  props: {
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {}
 })
 </script>
 
 <template>
-    <div class="description">
-      {{ title }}
-    </div>
+  <div class="description st-space-item">
+    {{ description }}
+  </div>
 </template>
 
 <style scoped>
-
 .description {
   height: 4rem;
   background-color: #fff;
   display: flex;
   align-items: center;
-  padding: 2rem;
   border-top-left-radius: 7px;
   border-top-right-radius: 7px;
 }
