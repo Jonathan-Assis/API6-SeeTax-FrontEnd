da79546832b172e6c0ca1b89567df255ad4666c7
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-30 12:13:39 -0300

    Adição do vuetify

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,18 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+  
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(vuetify)
 app.mount('#app')
 
 import './assets/stylesheet/global.css'
