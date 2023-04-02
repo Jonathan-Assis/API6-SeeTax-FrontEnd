46deafde79445adb5db9da094cb84ea5d158997a
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-04-02 03:46:16 -0300

    Criação da conexão com o axios

---
+++ src/services/index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const url = 'https://api6-seetax-backend.onrender.com';
+let method = axios.create({
+    baseURL: url,
+    timeout: 30000
+});
+
+export default class ServerConnection {
+    static async getRanking() {
+        return await method.get('/ranking');
+    }
+
+    static async getTaxa() {
+        return await method.get('/tax');
+    }
+}
