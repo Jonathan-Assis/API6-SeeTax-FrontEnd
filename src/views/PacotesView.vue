9ece60e3c80fdaf5fcde8056f3c27e8f0a44ef13
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-04-02 18:23:03 -0300

    Adição de exemplos na página de pacotes
    atualmente ocultada pois não faz parte da entrega

--- src/views/PacotesView.vue
+++ src/views/PacotesView.vue
@@ -1,19 +1,26 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { PhPackage } from '@phosphor-icons/vue'
+import { VDataTable } from 'vuetify/labs/VDataTable';
+import { VPagination, VTextField } from 'vuetify/components';
 
 import HeaderComponent from '../components/header/HeaderComponent.vue';
 import HeaderNav from '../components/header/HeaderNav.vue';
 import TableCommonComponent from '../components/table/TableCommonComponent.vue';
+import TableGroupingComponent from '../components/table/TableGroupingComponent.vue';
 
 export default defineComponent({
     name: "PacotesView",
     components: {
-        HeaderComponent,
-        HeaderNav,
-        TableCommonComponent,
-        PhPackage
-    },
+    HeaderComponent,
+    HeaderNav,
+    TableCommonComponent,
+    PhPackage,
+    VDataTable,
+    VPagination,
+    VTextField,
+    TableGroupingComponent
+},
     setup() {
         const header = ref([
             "Posição",
@@ -52,9 +59,189 @@ export default defineComponent({
                 media: "5,000"
             },
         ]);
+        
+        const headers = ref( [
+          {
+            key: 'name',
+            title: 'Dessert (100g serving)',
+          },
+          { title: 'Calories', key: 'calories' },
+          { title: 'Fat (g)', key: 'fat' },
+          { title: 'Carbs (g)', key: 'carbs' },
+          { title: 'Protein (g)', key: 'protein' },
+          { title: 'Iron (%)', key: 'iron' },
+        ])
+        const desserts = ref( [
+          {
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: 1,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: 1,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: 7,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: 8,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: 16,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: 0,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: 2,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: 45,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: 22,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: 6,
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: 1,
+          },
+          {
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: 7,
+          },
+          {
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: 8,
+          },
+          {
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: 16,
+          },
+          {
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: 0,
+          },
+          {
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: 2,
+          },
+          {
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: 45,
+          },
+          {
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: 22,
+          },
+          {
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: 6,
+          },
+        ])
+
+        
+        const page = ref(1)
+        const itemsPerPage = ref(5)
+
+        const options = ref({
+          pageCount: desserts.value.length / itemsPerPage.value,
+        })
+      
         return {
             header,
-            body
+            body,
+            options,
+            page,
+            itemsPerPage,
+            headers,
+            desserts,
         }
     }
 })
@@ -66,11 +253,12 @@ export default defineComponent({
         <HeaderNav to="/" routerName="Dashboard" />
         <HeaderNav to="/filtrar/pacotes" routerName="Pacotes" :actualRoute="true" />
     </HeaderComponent>
-    <TableCommonComponent title="Pacotes de serviços"
+
+<!--     <TableGroupingComponent
+        title="Pacotes de serviços"
         description="Pacotes de serviços oferecidos pelos bancos, valor mínimo e máximo cobradopela utilização dos serviços"
-        :header="header" :body="body">
-        <template v-slot:icon>
-            <PhPackage :size="34" class="st-icon-gray" weight="duotone" />
-        </template>
-    </TableCommonComponent>
+        :headerData="headers"
+        :bodyData="desserts"
+        >
+    </TableGroupingComponent> -->
 </template>
