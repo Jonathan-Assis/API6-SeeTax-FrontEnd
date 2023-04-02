f9241a27ab8c6cf985122fb7a1ac8a4f98a65693
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-04-02 18:20:16 -0300

    Adição de linguagem pt para o vuetify :tada:

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { pt } from 'vuetify/locale'
 import App from './App.vue'
 
 import { router } from './router'
@@ -21,11 +22,15 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
   components,
   directives,
+  locale: {
+    locale: 'pt',
+    messages: { pt }
+  }
 })
 
 app.use(vuetify)
 app.mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js"
-import './assets/stylesheet/global.css'
+import "./assets/stylesheet/global.css"
 
