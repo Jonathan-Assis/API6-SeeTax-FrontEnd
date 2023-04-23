d3d3229fd282b05b64e49d4a72e7309e6eefeef2
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-04-23 19:53:36 -0300

    Adição de constante do tipo de pessoa

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+import ServerConnection from '../services';
+
+export const tipoPessoa = [
+    {
+        key: 'F',
+        type: 'Pessoa Física'
+    },
+    {
+        key: 'J',
+        type: 'Pessoa Jurídica'
+    }
+]
