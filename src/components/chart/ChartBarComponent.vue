46877d8a3706b0f39c60c6b013aec5ff21610e14
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-05-16 03:50:06 -0300

    Correções no gráfico para escala logarítmica

--- src/components/chart/ChartBarComponent.vue
+++ src/components/chart/ChartBarComponent.vue
@@ -2,220 +2,168 @@
 /* import Chart, { type ChartConfiguration, type ChartData, type ChartItem } from 'chart.js'; */
 import { onMounted } from 'vue';
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, type ChartData, type ChartConfiguration, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'
+import { Chart as ChartJS, type ChartData, type ChartConfiguration, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, LogarithmicScale } from 'chart.js'
 
 import { tipoPessoa } from '@/constants'
 import { ref } from 'vue';
 import { PhMagnifyingGlass } from '@phosphor-icons/vue';
+import { useTarifasStore } from '@/stores';
+import { storeToRefs } from 'pinia';
 
 const pessoa = ref([])
 const pessoaNome = ref([])
 const grupo = ref([])
 const grupoNome = ref([])
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, )
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, LogarithmicScale)
 const chartData = ref({
-    labels: [],
-    datasets: []
+  labels: [],
+  datasets: []
 })
 const chartOptions = ref({
 
 })
 
 
-async function getSelects() {
-    const resp = await fetch(`https://olinda.bcb.gov.br/olinda/servico/Informes_ListaValoresDeServicoBancario/versao/v1/odata/GruposConsolidados?$top=100&$format=json`)
-    await resp.json()
-        .then((data) => {
-            grupo.value = data.value
-            data.value.forEach(value => grupoNome.value.push(value.Nome))
-        })
-    tipoPessoa.forEach((v) => {
-        pessoaNome.value.push(v.type)
-        pessoa.value.push(v)
+/* async function getSelects() {
+  const resp = await fetch(`https://olinda.bcb.gov.br/olinda/servico/Informes_ListaValoresDeServicoBancario/versao/v1/odata/GruposConsolidados?$top=100&$format=json`)
+  await resp.json()
+    .then((data) => {
+      grupo.value = data.value
+      data.value.forEach(value => grupoNome.value.push(value.Nome))
     })
-}
+  tipoPessoa.forEach((v) => {
+    pessoaNome.value.push(v.type)
+    pessoa.value.push(v)
+  })
+} */
 
-async function getMinMedMax(person: string, group: string, onMounted?: boolean) {
-    const resp = await fetch(`https://olinda.bcb.gov.br/olinda/servico/Informes_ListaValoresDeServicoBancario/versao/v1/odata/ListaValoresServicoBancario(PessoaFisicaOuJuridica=@PessoaFisicaOuJuridica,CodigoGrupoConsolidado=@CodigoGrupoConsolidado)?@PessoaFisicaOuJuridica='${person}'&@CodigoGrupoConsolidado='${group}'&$top=100&$format=json`)
-    await resp.json()
-        .then(data => {
-            let dados = data.value
-            const labels = dados.map(dado => {
-                const label = dado.NomeServico
-                return label
-            })
-            const valorMinimo = dados.map(dado => {
-                return dado.ValorMinimo
-            })
-            const valorMedia = dados.map(dado => {
-                return dado.ValorMedio
-            })
-            const valorMaximo = dados.map(dado => {
-                return dado.ValorMaximo
-            })
-            const periodicidadeValorMinimo = dados.map(dado => {
-                return { label: dado.NomeServico, periodicidade: dado.PeriodicidadeValorMinimo }
-            })
-            const periodicidadeValorMaximo = dados.map(dado => {
-                return { label: dado.NomeServico, periodicidade: dado.PeriodicidadeValorMaximo }
-            })
+async function getMaxServicosGrupoServico(group: string = '02', servico: string = '1101') {
+  handleMaxServicosPorGrupoServico(group, servico).then(() => {
+    let dados = tarifasStore.servicosMaxGrupoServico
+    const labels = dados.map(dado => {
+      const label = dado.RazaoSocial
+      return label
+    })
+    const valorMaximo = dados.map(dado => {
+      return dado.ValorMaximo
+    })
 
-            chartData.value = {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'Mínimo',
-                        /* indexAxis: 'x', */
-                        backgroundColor: '#00ed64',
-                        borderColor: '#00ed64',
-                        data: valorMinimo,
-                        borderRadius: 100,
-                    },
-                    {
-                        label: 'Média',
-                        /* indexAxis: 'x', */
-                        backgroundColor: '#FFB300',
-                        borderColor: '#FFB300',
-                        data: valorMedia,
-                        borderRadius: 100
-                    },
-                    {
-                        label: 'Máximo',
-                        /* indexAxis: 'x', */
-                        backgroundColor: '#3A57E8',
-                        borderColor: '#3A57E8',
-                        data: valorMaximo,
-                        borderRadius: 100,
-                    },
-                ],
-            }
+    const periodicidadeValorMaximo = dados.map(dado => {
+      return { label: dado.RazaoSocial, periodicidade: dado.Periodicidade }
+    })
 
-            chartOptions.value = {
-                options: {
-                   /*  scaleShowValues: true,
-                   maintainAspectRatio: false, */
-                  // responsive:true,
-                   scales: {
-                    axies:{
-                      y:{
-                        type: 'logarithmic',
-                      }
-                    },
-                    yAxes: [{
+    chartData.value = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Máximo',
+          indexAxis: 'y',
+          /* indexAxis: 'x', */
+          backgroundColor: '#3A57E8',
+          borderColor: '#3A57E8',
+          data: valorMaximo,
+          borderRadius: 100,
+        },
+      ],
+    }
 
-                      type: 'logarithmic',
-                    }],
-                    myScale: {
-        type: 'logarithmic',
+    chartOptions.value = {
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: 'Escala logarítmica',
+            font: {
+                size: 16,
+                weight: 'bold'
+            },
+          },
+          type: 'logarithmic',
+        },
       },
-                        x: {
-                          display: true,
-                        },
-                        y: {
-                          display: true,
-                          type: 'logarithmic',
-                        },
-                      
-                        /* alignToPixels: true,
-                        ticks: {
-                          autoSkip: false
-                        } */
-                      
-                     
-                    }
-                    },
-                plugins: {
-                    legend: {
-                        labels: {
-                            boxWidth: 10,
-                        }
-                    },
-                },
-                /* tooltip: {
-                    callbacks: {
-                        footer: (context) => {
-                            if (context[0].dataset.label == 'Mínimo') {
-                                const periodo = periodicidadeValorMinimo.find((value) => {
-                                    return value.label === context[0].label
-                                })
-                                return `Periodicidade: ${periodo.periodicidade}`
+      plugins: {
+        legend: {
+          font: {
+                size: 14,
+                weight: 'bold'
+            },
+          labels: {
+            font: {
+                size: 14,
+                weight: 'bold'
+            },
+            boxWidth: 10,
+            usePointStyle: true,
+          pointStyle: 'circle'
+          }
+        },
+      },
+      /* tooltip: {
+          callbacks: {
+              footer: (context) => {
+                      const periodo = periodicidadeValorMaximo.find((value) => {
+                          return value.label === context[0].label
+                      })
 
-                            }
-                            else if (context[0].dataset.label == 'Máximo') {
-                                const periodo = periodicidadeValorMaximo.find((value) => {
-                                    return value.label === context[0].label
-                                })
+                      return `Periodicidade: ${periodo.periodicidade}`
+              }
+          }
 
-                                return `Periodicidade: ${periodo.periodicidade}`
-                            }
-                        }
-                    }
-
-                }  */
-            }
-
-        })
-
+      }   */
+    }
+  })
 }
 
 
 async function searchData() {
-    let person = 'F'
-    let group = '01'
-    if (selectedPessoa.value) {
-        const pessoaChar = pessoa.value.find(a => {
-            return a.type == selectedPessoa.value
-        })
-        person = pessoaChar.key
-    }
-    if (selectedGrupo.value) {
-        const grupoCode = grupo.value.find(a => {
-            return a.Nome == selectedGrupo.value
-        })
-        group = grupoCode.Codigo
-    }
-    getMinMedMax(person, group)
+  let servico = '1101'
+  let group = '02'
+  if (selectedServico.value) {
+    const servicoCode = tarifasStore.servicos.find(a => {
+      return a.Nome == selectedServico.value
+    })
+    servico = servicoCode.Codigo
+  }
+  if (selectedGrupo.value) {
+    const grupoCode = tarifasStore.grupos.find(a => {
+      return a.Nome == selectedGrupo.value
+    })
+    group = grupoCode.Codigo
+  }
+  getMaxServicosGrupoServico(group, servico)
 }
-
+const tarifasStore = useTarifasStore()
+const { grupos, servicos } = storeToRefs(tarifasStore)
 onMounted(() => {
-    getSelects()
-    getMinMedMax('F', '01', true)
+  tarifasStore.getGrupoConsolidado()
+  tarifasStore.getServicos()
+  handleMaxServicosPorGrupoServico()
+  //getSelects()
+  getMaxServicosGrupoServico()
 })
 
-const selectedPessoa = ref('')
+const handleMaxServicosPorGrupoServico = async (grupo: string = '02', servico: string = '1101') => {
+  return await tarifasStore.getMaxServicosPorGrupoServico(grupo, servico);
+}
+const selectedServico = ref('')
 const selectedGrupo = ref('')
 </script>
 
 <template>
   <div class="st-space-item st-bg-white-primary st-shadow st-rounded">
-    <h4>Tarifas</h4>
+    <h4>Tarifas por Serviço</h4>
     <div class="header-title-actions">
       <div class="options">
         <div class="st-select">
-          Visualizar por:
-          <v-select
-            label="Selecionar tipo"
-            class="st-select-field"
-            v-model="selectedPessoa"
-            :items="pessoaNome"
-            single-line
-            variant="solo"
-            density="compact"
-          ></v-select>
+          Grupo Consolidado:
+          <v-select v-if="grupos.length" class="st-select-field" label="Selecionar grupo" v-model="selectedGrupo"
+            :items="grupos.map(e => e.Nome)" single-line variant="solo" density="compact"></v-select>
         </div>
         <div class="st-select">
-          Grupo:
-          <v-select
-            v-if="grupo.length"
-            class="st-select-field"
-            label="Selecionar grupo"
-            v-model="selectedGrupo"
-            :items="grupoNome"
-            single-line
-            variant="solo"
-            density="compact"
-          ></v-select>
+          Tipo do Serviço:
+          <v-select v-if="servicos.length" class="st-select-field" label="Selecionar grupo" v-model="selectedServico"
+            :items="servicos.map(e => e.Nome)" single-line variant="solo" density="compact"></v-select>
         </div>
         <v-btn @click="searchData()" class="st-btn st-rounded">
           Buscar
@@ -258,7 +206,6 @@ const selectedGrupo = ref('')
 
 .options {
   display: flex;
-  width: 45rem;
   gap: 1rem;
   justify-content: flex-end;
 }
