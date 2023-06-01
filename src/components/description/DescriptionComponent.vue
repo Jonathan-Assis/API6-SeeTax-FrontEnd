46b4062aa94a9688353e33612ff773096ef16e3a
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-06-01 11:09:29 -0300

    Correções no slot de descrição para uso na tabela e chart

--- src/components/description/DescriptionComponent.vue
+++ src/components/description/DescriptionComponent.vue
@@ -9,16 +9,29 @@ defineProps({
 
 <template>
   <div class="description st-bg-white-primary st-space-item">
-    {{ description }}
+    <p class="st-table-description">
+      {{ description }}
+    </p>
+    <div class="description-slot">
+      <slot name="right"></slot>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .description {
-  height: 4rem;
+ /*  height: 4rem; */
   display: flex;
+  width: 100%;
   align-items: center;
+  justify-content: space-between;
   border-top-left-radius: 7px;
   border-top-right-radius: 7px;
+}
+.description-slot {
+  display: flex;
+  align-items: flex-end;
+  width: inherit;
+  max-width: 20%;
 }
 </style>
