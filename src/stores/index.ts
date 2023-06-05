0841bc24172ecaa1839fb950c17e6128a94b3470
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-06-04 23:49:17 -0300

    Adição dos stores

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,39 +1,66 @@
-import type { Pessoa } from '@/interfaces/constants'
+import type { ICNPJ, IGruposConsolidado, IServicos, ITarifasServicosGrupoMax, ITarifasServicosInstituicaoMax, ITarifasServicosMinMedMaxValue } from './../interfaces/services/index';
+import type { TPessoa } from '@/interfaces/constants'
 import ServerConnection from '@/services'
 import { defineStore } from 'pinia'
 import dayjs from 'dayjs'
 
 export const useTarifasStore = defineStore('tarifasStore', {
   state: () => ({
-    grupos: [],
+    tipoPessoa: 'F' as TPessoa,
+    grupo: '02' as string,
+    grupos: [] as IGruposConsolidado[],
+    servico: '1101' as string,
     instituicoes: [],
-    servicos: [],
-    servicosMinMedMax: [],
-    servicosMaxInstituicao: [],
-    servicosMaxGrupoServico: [],
-    tarifas: [],
-    isLoading: false
+    instituicao: '92702067',
+    cnpj:[] as ICNPJ[],
+    servicos: [] as IServicos[],
+    servicosMinMedMax: [] as ITarifasServicosMinMedMaxValue[],
+    servicosMaxInstituicao: [] as ITarifasServicosInstituicaoMax[],
+    servicosMaxGrupoServico: [] as ITarifasServicosGrupoMax[],
+    isLoading: false as boolean
   }),
   getters: {},
   actions: {
+    async setTipoPessoa(tipo: TPessoa='F'){
+      this.tipoPessoa = tipo
+      
+      await this.getMinMedMaxServicos(tipo, this.grupo)
+      await this.getMaxServicosPorInstituicao(tipo, this.instituicao)
+    },
+    async setGrupo(grupo: string ='02'){
+      this.grupo = grupo
+
+      await this.getMinMedMaxServicos(this.tipoPessoa, grupo)
+      await this.getMaxServicosPorGrupoServico(grupo, this.servico)
+    },
+    async setServico(servico: string='1101'){
+      this.servico = servico
+
+      await this.getMaxServicosPorGrupoServico(this.grupo, servico)
+    },
+    async setInstituicao(instituicao: string = '92702067'){
+      this.instituicao = instituicao
+
+      await this.getMaxServicosPorInstituicao(this.tipoPessoa, instituicao)
+    },
     async getGrupoConsolidado() {
       try {
         this.isLoading = true
-        const { data } = await ServerConnection.getGrupos()
+        const resp = (await ServerConnection.getGrupos()).data
 
-        this.grupos = data.value
+        this.grupos = resp
       } catch (error) {
         console.log(error)
       } finally {
         this.isLoading = false
       }
     },
-    async getCNPJ(grupo: string) {
+    async getCNPJ(grupo: string = '02') {
       try {
         this.isLoading = true
         const { data } = await ServerConnection.getCNPJ(grupo)
 
-        this.instituicoes = data.value
+        this.cnpj = data.value
       } catch (error) {
         console.log(error)
       } finally {
@@ -43,20 +70,20 @@ export const useTarifasStore = defineStore('tarifasStore', {
     async getServicos() {
       try {
         this.isLoading = true
-        const { data } = await ServerConnection.getServicos()
+        const resp = await (await ServerConnection.getServicos()).data
 
-        this.servicos = data.value
+        this.servicos = resp
       } catch (error) {
         console.log(error)
       } finally {
         this.isLoading = false
       }
     },
-    async getMinMedMaxServicos(tipo: Pessoa, grupoConsolidado: string) {
+    async getMinMedMaxServicos(tipo: TPessoa='F', grupoConsolidado: string='02') {
       try {
         this.isLoading = true
         const { data } = await ServerConnection.getMinMedMaxServicos(tipo, grupoConsolidado)
-
+       
         this.servicosMinMedMax = data.value
       } catch (error) {
         console.log(error)
@@ -64,7 +91,7 @@ export const useTarifasStore = defineStore('tarifasStore', {
         this.isLoading = false
       }
     },
-    async getMaxServicosPorInstituicao(tipo: Pessoa, cnpj: string) {
+    async getMaxServicosPorInstituicao(tipo: TPessoa = 'F', cnpj: string = '92702067') {
       try {
         this.isLoading = true
         const { data } = await ServerConnection.getMaxServicosPorInstituicao(tipo, cnpj)
